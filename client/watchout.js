efe55f06a7d4ad031cec2603624df31cc23bee8d
Conor C <Careyc@union.edu>
2016-06-17 14:53:35 -0700

    got a background image and score counter

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,24 @@
 // start slingin' some d3 here.
+var updateScore = function(start) {
+  var score = start;
+  setInterval(function() {
+    d3.select('body').select('.current').text('Current score: ' + score);
+    score++;
+  }, 100);
+  console.log(d3.select('body').select('.current'));
+};
+
+var asteroids = function() {
+  var xPos = Math.random();
+  console.log(d3.select('body'));
+  d3.selectAll("asteroid")
+    .data(data)
+  .enter().append("asteroid")
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", 2.5);
+}
+
+
+updateScore(0);
+asteroids();
